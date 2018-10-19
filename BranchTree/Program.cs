2f7a748c8c041b17d7504dec823bc943d1e7d1f4
Kuroshi117 <phoebehana4@gmail.com>
2018-10-19 15:11:58 -0500

    Added FindLeaves(), FindInternalNodes. Modified Inputs() a lot.

--- BranchTree/Program.cs
+++ BranchTree/Program.cs
@@ -14,18 +14,12 @@ namespace BranchTree
         public static bool IsReady = false;
         static void Main(string[] args)
         {
-            LoadText(@"C:\Users\Potato\Downloads\people.txt", Text);
+            LoadText(@"C:\workspace\treeTestData\people.txt", Text);
             if (IsReady == true)
             {
-                //SortTree(Text, Nodes);
-                //SortTreeOther(Text, Nodes);
+
                 SortBranchTree(Text, Nodes);
                 ReadNodes(Nodes);
-                //GetNode("");
-                //GetNodes("");
-                //GetLeaves();
-                //GetInternalNodes();
-                //WriteOutlineFile(Nodes);
                 Inputs();
             }
 
@@ -185,11 +179,258 @@ namespace BranchTree
             return GotNodes;
         }
 
-        
+        public static List<Tree> FindLeaves()
+        {
+            List<Tree> GotNodes = new List<Tree>();
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                if (!Nodes[i].Children.Any())
+                {
+                    GotNodes.Add(Nodes[i]);
+
+                }
+
+            }
+
+            return GotNodes;
+        }
+
+        public static List<Tree> FindInternalNodes()
+        {
+            List<Tree> GotNodes = new List<Tree>();
+            for (int i = 0; i < Nodes.Count; i++)
+            {
+                if (Nodes[i].Parent == null && !Nodes[i].Children.Any())
+                {
+                    GotNodes.Add(Nodes[i]);
+
+                }
+
+            }
+
+            return GotNodes;
+        }
 
 
 
-        public static void SortTree(List<string> text, List<Tree> nodes)
+        public static int NumberOfOcc(string text, string pattern)
+        {
+            int count = 0;
+            int i = 0;
+            while ((i = text.IndexOf(pattern, i)) != -1)
+            {
+                i += pattern.Length;
+                count++;
+            }
+            return count;
+        }
+
+        public static void SetDepth(string id)
+        {
+            Nodes.Find(tree => tree.id() == id).Depth = Nodes.Find(tree => tree.id() == id).Parent.Depth + 1;
+            if(Nodes.Find(tree => tree.id() == id).Children != null)
+            {
+                foreach (Tree t in Nodes.Find(tree => tree.id() == id).Children)
+                {
+                    SetDepth(t.id());
+                }
+            }
+        }
+
+        public static void ReadNodes(List<Tree> nodes)
+        {
+
+            List<Tree> Roots = new List<Tree>();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (nodes[i].Depth == 0)
+                {
+                    Roots.Add(nodes[i]);
+                }
+            }
+            foreach (Tree t in Roots)
+            {
+                ReadChildren(t);
+            }
+
+        }
+
+        public static void ReadChildren(Tree node)
+        {
+            string tabs;
+            tabs = new string('\t', node.Depth);
+            Console.WriteLine(tabs + node.Content()+", "+node.id());
+            if (node.Children != null)
+            {
+                foreach (Tree t in node.Children)
+                {
+                    ReadChildren(t);
+                }
+            }
+        }
+
+        public static void WriteOutlineFile(List<Tree> nodes, string filename)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\workspace\"+filename+".txt"))
+            {
+                //basic way
+                /*string tabs;
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    tabs = new string('\t', nodes[i].Depth);
+                    sw.WriteLine(tabs + nodes[i].Content());
+                }*/
+
+                //hierarichal way
+                List<Tree> Roots = new List<Tree>();
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    if (nodes[i].Depth == 0)
+                    {
+                        Roots.Add(nodes[i]);
+                    }
+                }
+                foreach (Tree t in Roots)
+                {
+                    WriteChildren(t, sw);
+                }
+
+            }
+        }
+
+        public static void WriteChildren(Tree node, StreamWriter sw)
+        {
+            string tabs;
+            tabs = new string('\t', node.Depth);
+            sw.WriteLine(tabs + node.Content()+", "+ node.id());
+            if (node.Children != null)
+            {
+                foreach (Tree t in node.Children)
+                {
+                    WriteChildren(t, sw);
+                }
+            }
+        }
+
+        public static void Inputs()
+        {
+            Console.WriteLine("\n---INPUTS--- \nadd parentID,nameOfNewNode \nremove nodeToRemoveID \nmove nodeToMoveID,newParentID \nget ID \nget name \nget leaves \nget internal \nwrite fileName");
+            string inputString = "";
+            string[] inputArray = new string[4];
+            char[] c = new char[]{ ' ', ',' };
+            while(true)
+            {
+                inputString = Console.ReadLine();
+                inputArray = inputString.Split(c, StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("\n");
+
+                if (inputArray[0].Equals("add", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    AddNewNode(inputArray[2], inputArray[1]);
+                    ReadNodes(Nodes);
+                }
+                else if (inputArray[0].Equals("remove", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    DeleteNode(inputArray[1]);
+                    ReadNodes(Nodes);
+                }
+                else if (inputArray[0].Equals("move", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    MoveNode(inputArray[1], inputArray[2]);
+                    ReadNodes(Nodes);
+                }
+                else if (inputArray[0].Equals("get", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    
+                    if(inputArray.Length <= 1)
+                    {
+                        Console.WriteLine("Nothing to find. ");
+                        continue;
+                    }
+
+                    if (inputArray[1].Equals("leaves", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        List<Tree> tempTreeList = FindLeaves();
+                        if (tempTreeList.Any())
+                        {
+                            Console.WriteLine("Found: ");
+                            foreach (Tree t in tempTreeList)
+                            {
+                                Console.WriteLine(t.Content() + ", " + t.id());
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No leaves found.");
+                        }
+                    }
+                    else if (inputArray[1].Equals("internal", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        List<Tree> tempTreeList = FindInternalNodes();
+                        if (tempTreeList.Any())
+                        {
+                            Console.WriteLine("Found: ");
+                            foreach (Tree t in tempTreeList)
+                            {
+                                Console.WriteLine(t.Content() + ", " + t.id());
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No internal nodes found.");
+                        }
+                    }
+
+                    if (FindNode(inputArray[1]) != null)
+                    {
+                        Tree tempTree = FindNode(inputArray[1]);
+                        Console.WriteLine("Found:\n" + tempTree.Content() + ", " + tempTree.id());
+
+                    }
+                    else if (FindNodebyContent(inputArray[1]) != null)
+                    {
+                        List<Tree> tempTreeList = FindNodebyContent(inputArray[1]);
+                        if (tempTreeList.Any())
+                        {
+                            Console.WriteLine("Found: ");
+                            foreach (Tree t in tempTreeList)
+                            {
+                                Console.WriteLine(t.Content() + ", " + t.id());
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(inputArray[1] + " not found.");
+                    }
+
+                    
+                }
+                else if (inputArray[0].Equals("write", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    WriteOutlineFile(Nodes, inputArray[1]);
+                    Console.WriteLine(inputArray[1]+".txt was written.");
+                }
+                else if(inputArray[0].Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Exiting program.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid input");
+                }
+                
+            }
+        }
+
+
+
+        /*public static void SortTree(List<string> text, List<Tree> nodes)
         {
             string tempName;
             for (int i = 0; i < text.Count; i++)
@@ -375,187 +616,7 @@ namespace BranchTree
 
             }
             return GotNodes;
-        }
-
-
-
-
-        
-
-        public static int NumberOfOcc(string text, string pattern)
-        {
-            int count = 0;
-            int i = 0;
-            while ((i = text.IndexOf(pattern, i)) != -1)
-            {
-                i += pattern.Length;
-                count++;
-            }
-            return count;
-        }
-
-        public static void SetDepth(string id)
-        {
-            Nodes.Find(tree => tree.id() == id).Depth = Nodes.Find(tree => tree.id() == id).Parent.Depth + 1;
-            if(Nodes.Find(tree => tree.id() == id).Children != null)
-            {
-                foreach (Tree t in Nodes.Find(tree => tree.id() == id).Children)
-                {
-                    SetDepth(t.id());
-                }
-            }
-        }
-
-        public static void ReadNodes(List<Tree> nodes)
-        {
-
-            List<Tree> Roots = new List<Tree>();
-            for (int i = 0; i < nodes.Count; i++)
-            {
-                if (nodes[i].Depth == 0)
-                {
-                    Roots.Add(nodes[i]);
-                }
-            }
-            foreach (Tree t in Roots)
-            {
-                ReadChildren(t);
-            }
-
-        }
-
-        public static void ReadChildren(Tree node)
-        {
-            string tabs;
-            tabs = new string('\t', node.Depth);
-            Console.WriteLine(tabs + node.Content()+", "+node.id());
-            if (node.Children != null)
-            {
-                foreach (Tree t in node.Children)
-                {
-                    ReadChildren(t);
-                }
-            }
-        }
-
-        public static void WriteOutlineFile(List<Tree> nodes)
-        {
-            using (StreamWriter sw = new StreamWriter(@"C:\workspace\WriteHierachy.txt"))
-            {
-                //basic way
-                /*string tabs;
-                for (int i = 0; i < nodes.Count; i++)
-                {
-                    tabs = new string('\t', nodes[i].Depth);
-                    sw.WriteLine(tabs + nodes[i].Content());
-                }*/
-
-                //hierarichal way
-                List<Tree> Roots = new List<Tree>();
-                for (int i = 0; i < nodes.Count; i++)
-                {
-                    if (nodes[i].Depth == 0)
-                    {
-                        Roots.Add(nodes[i]);
-                    }
-                }
-                foreach (Tree t in Roots)
-                {
-                    WriteChildren(t, sw);
-                }
-
-            }
-        }
-
-        public static void WriteChildren(Tree node, StreamWriter sw)
-        {
-            string tabs;
-            tabs = new string('\t', node.Depth);
-            sw.WriteLine(tabs + node.Content());
-            if (node.Children != null)
-            {
-                foreach (Tree t in node.Children)
-                {
-                    WriteChildren(t, sw);
-                }
-            }
-        }
-
-        public static void Inputs()
-        {
-            string inputString = "";
-            string[] inputArray = new string[4];
-            while(true)
-            {
-                inputString = Console.ReadLine();
-                inputArray = inputString.Split(' ', ',');
-                Console.WriteLine("\n");
-                if (inputArray[0].Equals("add", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    AddNewNode(inputArray[2], inputArray[1]);
-                    ReadNodes(Nodes);
-                }
-                else if (inputArray[0].Equals("remove", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    DeleteNode(inputArray[1]);
-                    ReadNodes(Nodes);
-                }
-                else if (inputArray[0].Equals("move", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    MoveNode(inputArray[1], inputArray[2]);
-                    ReadNodes(Nodes);
-                }
-                else if (inputArray[0].Equals("get", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    
-                    if(inputArray.Length <= 1)
-                    {
-                        Console.WriteLine("Nothing to find. ");
-                        continue;
-                    }
-
-                    if (FindNode(inputArray[1]) != null)
-                    {
-                        Tree tempTree = FindNode(inputArray[1]);
-                        Console.WriteLine("Found:\n" + tempTree.Content() + ", " + tempTree.id());
-
-                    }
-                    else if (FindNodebyContent(inputArray[1]) != null)
-                    {
-                        List<Tree> tempTreeList = FindNodebyContent(inputArray[1]);
-                        if (tempTreeList.Any())
-                        {
-                            Console.WriteLine("Found: ");
-                            foreach (Tree t in tempTreeList)
-                            {
-                                Console.WriteLine(t.Content() + ", " + t.id());
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Not found.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine(inputArray[1] + " not found.");
-                    }
-
-                    
-                }
-                else if(inputArray[0].Equals("exit", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    Console.WriteLine("Exiting program.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Please enter valid input");
-                }
-                
-            }
-        }
-        
+        }*/
     }
 
     class Tree: INode
